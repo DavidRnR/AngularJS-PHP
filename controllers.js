905129f5fad02e21c18d42661324e5fe72efa210
DavidRnR <carlosdavidmartin@gmail.com>
2016-12-27 11:46:51 -0300

    Registro de nuevo usuario

--- controllers.js
+++ controllers.js
@@ -13,14 +13,37 @@ empleadoControllers.controller('EmpleadoListadoCtrl', ['$scope','$http', functio
       $scope.profesiones = r.data;
     });
   }
-
+  profesiones();
   $scope.eliminar = function (id) {
-    console.log(id);
-    $http.get('http://localhost/proyectos/AngularJS-PHP/api/?a=eliminar&id='+id).then(function () {
-      empleados();
-    });
+    if(confirm('Está seguro desea eliminar este empleado?')) {
+      $http.get('http://localhost/proyectos/AngularJS-PHP/api/?a=eliminar&id='+id).then(function () {
+        empleados();
+      });
+    }
   }
 
+  $scope.registrar = function () {
+  var nuevoEmpleado = {
+      Correo : $scope.Correo,
+      Nombre: $scope.Nombre,
+      Apellido: $scope.Apellido,
+      Sexo: $scope.Sexo,
+      Sueldo: $scope.Sueldo,
+      Profesion_id: $scope.Profesion_id,
+      FechaNacimiento: $scope.FechaNacimiento
+    }
+    console.log(nuevoEmpleado);
+    $http.post('http://localhost/proyectos/AngularJS-PHP/api/?a=registrar',nuevoEmpleado).then(function () {
+      empleados();
+      $scope.Correo = null;
+      $scope.Nombre =null;
+      $scope.Apellido=null;
+      $scope.Sexo=null;
+      $scope.Sueldo=null;
+      $scope.Profesion_id=null
+      $scope.FechaNacimiento=null;
+    });
+  }
 }]);
 
 empleadoControllers.controller('EmpleadoVerCtrl', ['$scope', '$routeParams', '$location', '$http', function ($scope, $routeParams, $location,$http) {
